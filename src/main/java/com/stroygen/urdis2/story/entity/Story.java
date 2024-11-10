f5809f3307810320ef83684edffcada9283fc204
kkkkimtaehyeon <kth1927@naver.com>
2024-11-10 23:15:25 +0900

    엔티티 수정

--- src/main/java/com/stroygen/urdis2/story/entity/Story.java
+++ src/main/java/com/stroygen/urdis2/story/entity/Story.java
@@ -8,15 +8,15 @@ import java.time.LocalDateTime;
 public class Story {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long storyId;
+    private Long storyId;
 
-    @Column(name = "story_title", length = 50)
+    @Column(length = 50)
     private String storyTitle;
-    @Column(name = "story_source", length = 1000, nullable = false)
+    @Column(length = 1000, nullable = false)
     private String storySource;
     private LocalDateTime createdAt;
-    private long likeCount;
-    private long viewCount;
-    private boolean isPublic;
+    private Long likeCount;
+    private Long viewCount;
+    private Boolean isPublic;
 
 }
