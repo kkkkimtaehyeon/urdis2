8294444333b23bec01a332bc435da5f0c76bd7ea
kkkkimtaehyeon <kth1927@naver.com>
2024-11-23 01:04:16 +0900

    카카오 auth 로그인 기능 구현

--- src/main/java/com/stroygen/urdis2/common/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/stroygen/urdis2/common/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,15 +1,18 @@
 package com.stroygen.urdis2.common.handler;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -17,8 +20,20 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
     private final RedisTemplate<String, Object> sessionRedisTemplate;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        // TODO: 세션 추가
-        log.info("로그인 성공");
+        // 쿠키 추가
+        String sessionId = UUID.randomUUID().toString();
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        Cookie cookie = new Cookie("SESSIONID", sessionId);
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(60 * 60);
+        response.addCookie(cookie);
+        // redis에 세션 등록
+        String email = oAuth2User.getAttribute("email");
+        assert email != null;
+        sessionRedisTemplate.opsForHash().put("LoginSession:", sessionId, email);
+
+        //TODO: 가입한 회원인지 판별 (여기서 or customOAuth2Service에서)
+        log.info(String.format("LoginSession:%s:%s 등록", sessionId, email));
         response.sendRedirect("/");
     }
 }
